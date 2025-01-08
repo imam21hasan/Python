2a15bd3a199fee6464669a0fd27e7c2ef4cc4376
Imam Hasan Shawon <152065554+imam21hasan@users.noreply.github.com>
2025-01-09 01:13:17 +0600

    Add files via upload

---
+++ Basic Code/Conditional Statement/If_else.py
@@ -0,0 +1,18 @@
+a = 20
+b = 10
+if a > b:
+    print("Python")
+
+
+marks = 30
+if marks >= 33:
+    print("Passed")
+else:
+    print("Failed")
+
+
+n = 30
+if n % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
