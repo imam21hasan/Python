0dd839977cfac28c7c6c77e0f9e87952cd810971
Imam Hasan Shawon <152065554+imam21hasan@users.noreply.github.com>
2025-04-02 22:52:05 +0600

    Update Cls_Obj_2.py

--- Basic Code/OOP/Cls_Obj_2.py
+++ Basic Code/OOP/Cls_Obj_2.py
@@ -8,5 +8,7 @@ class Car:
         return f"{self.year} {self.brand} {self.model}"
 
 car1 = Car("BMW", "i8", 2025)
+car2 = Car("Ferrari", "SF90 Spider", 2024)
 
 print(car1.display_info())
+print(car2.display_info())
