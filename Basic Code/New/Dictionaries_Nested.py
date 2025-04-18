282c851218d441fa5c0e4be4e1c62e90a84cdc79
Imam Hasan Shawon <152065554+imam21hasan@users.noreply.github.com>
2025-04-19 00:03:15 +0600

    Add files via upload

---
+++ Basic Code/New/Dictionaries_Nested.py
@@ -0,0 +1,17 @@
+carCollection = {
+  "car1" : {
+    "name" : "BMW",
+    "year" : 2014
+  },
+  "car2" : {
+    "name" : "Bentley",
+    "year" : 2018
+  },
+  "car3" : {
+    "name" : "Bugatti",
+    "year" : 2024
+  }
+}
+
+print(carCollection["car1"])
+print(carCollection["car3"])
